0384e2eb469333cdf897b03fc76056f3b1a03cfa
Kyle Welch <welch@udel.edu>
2022-06-24 21:11:45 -0400

    Finished Bob and Alice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
 public class Main {
+    public static void main(String[] args ) {
 
-    public static void main(String[] args ){
+        String name = "";
 
+        System.out.println("What is your name? ");
+
+        Scanner scan = new Scanner(System.in);
+
+        name = scan.nextLine();
+
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Hello " + name + " how are you doing?");
+        } else {
+            System.out.println("You don't belong here!");
+        }
     }
 }
